656e54b881c2e24d86fdefa7b2b0ecf65ddf3168
Nasrisameh <nasri.sameh2016@gmail.com>
2024-01-20 22:13:39 +0100

    first commit

---
+++ Week3/Day1/Core/Prop-it-Up/prop_it_up/src/components/PersonCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PersonCard = props => {
+    const {firstName, lastName, hairColor} = props;
+
+    return (
+    <div>
+        <h1>
+        {props.lastName}, {props.firstName}
+        </h1>
+        <p>Age: {props.age}</p>
+        <p>Hair Color: {props.hairColor}</p>
+    </div>
+    );
+};
+export default PersonCard;
